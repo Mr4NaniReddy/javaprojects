db413451731bfe72ff8a35133c3bac22ff8e1f74
Mr4NaniReddy <dumpabhargav22@gmail.com>
2023-05-20 08:14:34 +0530

    cyclicsort

--- cyclicsort.java
+++ cyclicsort.java
@@ -1,5 +1,38 @@
+import java.util.*;
+import java.util.Scanner;
+
 public class cyclicsort{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for(int  i=0;i<n;i++){
+            arr[i] = input.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println("SORTED:");
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+        
     }
+
+    
+   public static void sort(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct])
+            {
+                swap(arr,i,correct);
+            }
+            else{
+                 i++;
+            }
+        }
+    }
+    static void swap(int[] arr, int first, int correct){
+        int temp = arr[first];
+        arr[first] = arr[correct];
+        arr[correct] =temp;
+    } 
 }
